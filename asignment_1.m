2d631e1e79b4e34ffe9b92a79b497e961165e0b6
ringf <fredrikjring@gmail.com>
2017-09-04 12:01:43 +0200

    skapat patterns

--- asignment_1.m
+++ asignment_1.m
@@ -1,4 +1,16 @@
 %% 1
+pmax = 400; % Maximum number of patterns
+N = 200; % Number of bits
+
+patterns = randi([0,1],pmax,N); % Generate random patterns
+for x=1:pmax
+    for y=1:N
+        if (patterns(x,y)==0)               % Change 0 to -1
+            patterns(x,y)=-1;
+        end
+    end
+end
+
 
 %1 a)
 
