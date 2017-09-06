eb3c44f773080675825fea5d26130502342d7558
ringf <fredrikjring@gmail.com>
2017-09-06 10:15:21 +0200

    idea for more efficient Hopfield

--- Hopfield.m
+++ Hopfield.m
@@ -3,5 +3,8 @@ function new_state = Hopfield( weights, states )
 new_state = sign(weights*states);
 new_state(find(new_state==0),1) = 2*randi([0,1],length(find(new_state==0)),1) - 1;
 
+%new_state ==0 för en vektor med ettor där det är nollor. 1 -> +/- 1 och
+%matrisaddition så är samma operation fixad.
+
 end
 
