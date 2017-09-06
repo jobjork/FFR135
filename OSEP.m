989bff6e830677160521b626d94c955406f5e75c
ringf <fredrikjring@gmail.com>
2017-09-06 09:38:22 +0200

    main file woopdeedo

--- OSEP.m
+++ OSEP.m
@@ -1,8 +1,4 @@
-%function p_err = OSEP(p, N , numBits)
-
-p = 610;
-N = 10000;
-num_bits = 10000;
+function p_err = OSEP(p, N , num_bits)
 
 patterns = GeneratePatterns(p, N);
 
@@ -10,19 +6,6 @@ weights = zeros(N,N);
 for k=1:p
     weights = weights + 1/N*patterns(:,k) * patterns(:,k)';
 end
-
-%{
-for i=1:N
-    weights(i,i) = 0;err_vec
-end
-%}
-
-avg_errors = 0;
-count = 0;
-
-%% TODO: Add all the new_state(index)~=state(index)s in a long vector,
-% sample 10000 from this vector and form the p_err
-
 
 iterations = ceil(num_bits/(p*N));
 error_vector = zeros(p*N,iterations);
@@ -36,33 +19,7 @@ for i = 1:iterations
 end
 error_vector=error_vector(:); 
 
+index = randperm(length(error_vector));
+p_err = sum(error_vector(index(1:num_bits)))/num_bits;
 
-%%
-
-if p*N >= num_bits
-    index = randperm(1:(p*N));
-    index = index(1:num_bits);
-    
-    count = count + 1;
-    state_ind = ceil(p*rand);
-    state = patterns(:, state_ind);
-    new_state = sign(weights*state);
-    avg_errors = avg_errors + sum(new_state(index)~=state(index))/N;
-    num_bits = num_bits - p*N;
-else  
-    while num_bits > 0
-        
-        count = count + 1;
-        state_ind = ceil(p*rand);
-        state = patterns(:, state_ind);
-        new_state = sign(weights*state);
-        avg_errors = avg_errors + sum(new_state~=state)/N;
-        num_bits = num_bits - p*N;
-    end
-    
 end
-
-
-p_err = avg_errors/count
-%end
-
--- asignment_1.m
+++ asignment_1.m
@@ -1,11 +1,14 @@
 %% 1
-pmax = 400; % Maximum number of patterns
-N = 200; % Number of bits
-
-
-
-%1 a)
-
 %1 b)
 
+p = [1, (20:20:400)]';
+N = 200;
+num_bits = 10000;
 
+p_err = zeros(length(p),1);
+
+for i=1:length(p)
+   p_err(i) = OSEP(p(i), N, num_bits);
+end
+
+plot(p/N,p_err,'*')
