94e61b90bd30f269a00c2f147a96508ac826389e
ringf <fredrikjring@gmail.com>
2017-09-12 10:31:33 +0200

    fixed probabilities

--- StochasticUpdate.m
+++ StochasticUpdate.m
@@ -2,7 +2,7 @@ function update = StochasticUpdate( beta, state, weights )
 
 b = Hopfield(weights, state);
 g = Sigmoid(b,2*beta);
-update = 2*(rand(length(state),1)>g)-1;
+update = 2*(rand(length(state),1)<g)-1;
 
 end
 
